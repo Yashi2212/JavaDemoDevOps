4083bb91246fd9d03b49e60dd8481da83d295e8f
Yashi2212 <73119414+Yashi2212@users.noreply.github.com>
2024-04-01 21:59:16 +0530

    Add files via upload

---
+++ javaDemoDev/src/javaDemoDev/DemoJava.java
@@ -0,0 +1,7 @@
+package javaDemoDev;
+
+public class DemoJava {
+public static void main(String[] args) {
+	System.out.println("Hello World, This is Simple Java App");
+}
+}
---
+++ javaDemoDev/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author yashishukla
+ *
+ */
+module javaDemoDev {
+}
